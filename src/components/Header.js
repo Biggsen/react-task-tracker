4e2a5ee389f20d0bd6ce677be32083233bdf601f
Biggs Thorarensen <biggs@devillion.com>
2021-05-27 09:45:13 +0100

    Header using Button component. Proptypes added and onClick.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
-        <header>
+        <header className="header">
             <h1>{title}</h1>
+            <Button colour="green" text="Add" onClick={ onClick } />
         </header>
     )
 }
 
 Header.defaultProps = {
     title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string
 }
 
 export default Header
