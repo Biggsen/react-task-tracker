7b551813c3f0983ac67e42eda3b906381e425b2e
Biggs Thorarensen <biggs@devillion.com>
2021-05-27 09:46:07 +0100

    App has tasks data and uses Tasks component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
+const App = () => {
+    const [tasks, setTasks] = useState(
+        [
+            {
+                    id: 1,
+                    text: 'Do it now',
+                    day: 'May 26th at 4:35pm',
+                    reminder: false,
+            },
+            {
+                    id: 2,
+                    text: 'Maybe later',
+                    day: 'May 26th at 4:35pm',
+                    reminder: false,
+            },
+            {
+                    id: 3,
+                    text: 'Urgent',
+                    day: 'May 26th at 4:35pm',
+                    reminder: false,
+            },
+        ]
+    )
+
+    return (
+        <div className="container">
+            <Header />
+            <Tasks tasks={tasks} />
+        </div>
+    );
 }
 
 export default App;
