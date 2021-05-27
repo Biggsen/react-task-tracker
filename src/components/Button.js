e16f6e96359d46bcd72fec31588941b36d6d9fff
Biggs Thorarensen <biggs@devillion.com>
2021-05-27 09:43:59 +0100

    Button, Task and Tasks components

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+const Button = ({ colour, text, onClick }) => {
+    return (
+        <button
+            onClick={ onClick }
+            style={{ backgroundColor: colour }}
+            className="btn">
+                { text }
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    colour: 'steelblue',
+    text: 'Text'
+}
+
+Button.propTypes = {
+    colour: PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func
+}
+
+export default Button
